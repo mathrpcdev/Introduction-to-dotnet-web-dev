4249072b18224791912f94f94024cf4643b0096d
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-12-28 12:44:41 -0300

    Adicionada a solução da Microsoft Learn
    Eu tinha esquecido de colocar ao final do código.

--- MathPowAreaDoCirculo.cs
+++ MathPowAreaDoCirculo.cs
@@ -5,3 +5,10 @@ double areaDoCirculo = Math.Pow('double', 'double') * Math.PI; */
 double radiano = 2.50;
 double areaDoCirculo = Math.Pow(radiano, 2) * Math.PI;
 Console.WriteLine($"A área do círculo é: {areaDoCirculo}");
+
+
+// --> Microsoft Learn Solution
+
+double radius = 2.50;
+double area = Math.PI * radius * radius;
+Console.WriteLine(area);
