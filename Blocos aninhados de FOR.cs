78733c55396209a0bed8afe22779bca33872fd96
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-12-28 15:06:35 -0300

    first commit

---
+++ Blocos aninhados de FOR.cs
@@ -0,0 +1,17 @@
+﻿for (int row = 1; row < 11; row++)
+{
+    Console.WriteLine($"The row is: {row}");
+}
+
+for (char column = 'a'; column < 'k'; column++)
+{
+    Console.WriteLine($"The column is: {column}");
+}
+
+for (int fileira = 1; fileira < 11; fileira++)
+{
+    for (char coluna = 'a'; coluna < 'k'; coluna++)
+    {
+        Console.WriteLine($"The cell(pilha?) is: ({fileira}, {coluna})");
+    }
+}
