be73f50091b46b49c8cb3aac18f160d855e6e280
Matheus Rodrigues Cavalcante <96153654+mathrpcdev@users.noreply.github.com>
2022-12-30 20:39:11 -0300

    First commit

---
+++ Visibilidade de código em bloco aninhado.cs
@@ -0,0 +1,10 @@
+﻿int soma = 0;
+for (int numero = 1; numero < 21; numero++)
+{
+    if (numero % 3 == 0)
+    {
+        soma = soma + numero;
+    }
+}
+Console.WriteLine($"A soma dos números divisíveis por 3 é: {soma}");
+
